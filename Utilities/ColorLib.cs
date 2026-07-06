e134085bbd2e7997e68b4d2f881888d77fda4322
zj71 <154486251+skids-zj71@users.noreply.github.com>
2026-07-06 11:35:51 -0700

    sherbet glizzeh update

--- Utilities/ColorLib.cs
+++ Utilities/ColorLib.cs
@@ -325,8 +325,8 @@ namespace Elixir.Utilities
         }
 
         public static Material fmby = Url2Mat("https://i.ebayimg.com/images/g/XI8AAOSwwvRlMHkz/s-l1200.jpg");
-        public static Material SherbMat = Url2Mat("https://raw.githubusercontent.com/Cha554/Stone-Networking/main/Stone/Sherbert.jpg");
-        public static Material AngrySherbMat = Url2Mat("https://raw.githubusercontent.com/Cha554/Stone-Networking/main/Sherbert(1).jpg");
+        public static Material SherbMat = Url2Mat("https://raw.githubusercontent.com/menker-cs/Elixir/main/Resources/sherbert.jpg");
+        public static Material AngrySherbMat = Url2Mat("https://raw.githubusercontent.com/menker-cs/Elixir/main/Resources/sherbert(1).jpg");
 
         #region Menu Colors
         public static Material[] MenuMat = new Material[]
